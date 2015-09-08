9f88e8836dd9f33fed4e6560443486812d6cfaec
franpham <franpham@gmail.com>
2015-09-08 11:42:31 -1000

    added comments for including json data

--- email-check.js
+++ email-check.js
@@ -1,7 +1,10 @@
+// if testing with node, require the json file directly and assign it to a variable;
+// if testing with browser, assign the json object to a variable inside the data file and rename that file to '.js'; then
+// include the data.js file in a script tag before the script.js file in the html source;
+
 var emailData = require('./email-log.json');
 
 var emailModule = (function (log) {
-
   return {
     findUnique : function () {
       var sorted = log.emails.map(function (current) {
@@ -24,8 +27,8 @@ var emailModule = (function (log) {
     }
   };
 })(emailData);
-var emailFix = emailModule.findUnique();
 
-for (key in emailFix) {
+var emailFix = emailModule.findUnique();
+for (var key in emailFix) {
   console.log(key + ' was emailed ' + emailFix[key] + ' times');
 }
